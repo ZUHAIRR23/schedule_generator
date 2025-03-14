fd9de7854f725fa8c68fdcc4f036ce150725d765
Abdurrahman Zuhair <abdurrahman.zuh@gmail.com>
2025-03-14 10:49:54 +0700

    tes

--- lib/ui/gemini_page.dart
+++ lib/ui/gemini_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:intl/intl.dart';
 
 class GeminiPage extends StatefulWidget {
   const GeminiPage({super.key});
@@ -8,6 +10,46 @@ class GeminiPage extends StatefulWidget {
 }
 
 class _GeminiPageState extends State<GeminiPage> {
+  final _controllerName = TextEditingController();
+  final _controllerDuration = TextEditingController();
+  String _selectedPriority = 'High';
+  DateTime? _fromDate;
+  DateTime? _untilDate;
+  String _result = "";
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    dotenv.load();
+  }
+
+  Future<void> _selectDate(BuildContext context, bool isForm) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+    if (picked != null) {
+      setState(() {
+        if (isForm) {
+          _fromDate = picked;
+        } else {
+          _untilDate = picked;
+        }
+      });
+    }
+  }
+
+  Future<void> generateSchedule() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    final DateFormat formatter = DateFormat('yyy-MM-dd');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,10 +59,63 @@ class _GeminiPageState extends State<GeminiPage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Text("Welcome to the Schedule Generator"),
+            _textField("Schedule Name", _controllerName),
+            SizedBox(height: 10),
+            _dropDown(),
+            SizedBox(height: 10),
+            _textField("Duration", _controllerDuration, isNumber: true),
+            SizedBox(height: 10),
+            _datePicker(
+                "From Date", _fromDate, () => _selectDate(context, true)),
+            SizedBox(height: 10),
+            _datePicker(
+                "Until Date", _untilDate, () => _selectDate(context, false))
           ],
         ),
       ),
+    );
+  }
+
+  Widget _textField(
+    String label,
+    TextEditingController controller, {
+    bool isNumber = false,
+  }) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(labelText: label),
+      keyboardType: isNumber ? TextInputType.number : TextInputType.text,
+    );
+  }
+
+  Widget _dropDown() {
+    return DropdownButtonFormField<String>(
+      value: _selectedPriority,
+      items: ["High", "Medium", "Low"]
+          .map(
+            (element) => DropdownMenuItem(
+              value: element,
+              child: Text(element),
+            ),
+          )
+          .toList(),
+      onChanged: (String? value) {
+        setState(() {
+          _selectedPriority = value!;
+        });
+      },
+      decoration: InputDecoration(labelText: "Priority"),
+    );
+  }
+
+  Widget _datePicker(String label, DateTime? date, VoidCallback onTap) {
+    return ListTile(
+      title: Text(
+        date == null
+            ? '$label pilih tanggal'
+            : "$label ${DateFormat('yyyy-MM-dd').format(date)}",
+      ),
+      onTap: onTap,
     );
   }
 }
